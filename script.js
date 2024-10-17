593e935ffbd8f6104a0ff4d8f7586369d181f5d9
Islam Salah <islam.salah.is08@gmail.com>
2024-10-17 18:39:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// Q1
+
+// function filterEvenNumbers(arr) {
+//     return arr.filter(num => num % 2 === 0);
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const evenNumbers = filterEvenNumbers(numbers);
+// console.log(evenNumbers);
+
+// .....................................................................................
+
+// Q2
+
+    // function findLargestNumber(arr) {
+    //     return Math.max(...arr);
+    // }
+    
+    // // Example usage
+    // const numbers = [10, 45, 32, 89, 3];
+    // const largestNumber = findLargestNumber(numbers);
+    // console.log(largestNumber);
+
+
+// .....................................................................................
+
+// Q3
+
+    // function reverseString(str) {
+    //     return str.split('').reverse().join('');
+    // }
+    
+    // // Example usage
+    // const input = "hello";
+    // const reversed = reverseString(input);
+    // console.log(reversed);
+
+    // .....................................................................................
+
+// Q4
+
+// function removeDuplicates(arr) {
+//     return [...new Set(arr)];
+// }  
+//     const numbers = [1, 2, 3, 2, 4, 1, 5];
+//     const uniqueNumbers = removeDuplicates(numbers);
+//     console.log(uniqueNumbers); // Output: [1, 2, 3, 4, 5]
