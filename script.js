b57b17c696471b3ed315580d0a5d625fe8beec6f
Islam Salah <islam.salah.is08@gmail.com>
2024-10-17 19:21:14 +0300

    first commit

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 // Q1
 
-// function filterEvenNumbers(arr) {
-//     return arr.filter(num => num % 2 === 0);
-// }
+function filterEvenNumbers(arr) {
+    return arr.filter(num => num % 2 === 0);
+}
 
-// const numbers = [1, 2, 3, 4, 5, 6];
-// const evenNumbers = filterEvenNumbers(numbers);
-// console.log(evenNumbers);
+const numbers1 = [1, 2, 3, 4, 5, 6];
+const evenNumbers = filterEvenNumbers(numbers1);
+console.log(evenNumbers);
 
 // .....................................................................................
 
 // Q2
 
-    // function findLargestNumber(arr) {
-    //     return Math.max(...arr);
-    // }
+    function findLargestNumber(arr) {
+        return Math.max(...arr);
+    }
     
-    // // Example usage
-    // const numbers = [10, 45, 32, 89, 3];
-    // const largestNumber = findLargestNumber(numbers);
-    // console.log(largestNumber);
+    // Example usage
+    const numbers2 = [10, 45, 32, 89, 3];
+    const largestNumber = findLargestNumber(numbers2);
+    console.log(largestNumber);
 
 
 // .....................................................................................
 
 // Q3
 
-    // function reverseString(str) {
-    //     return str.split('').reverse().join('');
-    // }
+    function reverseString(str) {
+        return str.split('').reverse().join('');
+    }
     
-    // // Example usage
-    // const input = "hello";
-    // const reversed = reverseString(input);
-    // console.log(reversed);
+    // Example usage
+    const input = "hello";
+    const reversed = reverseString(input);
+    console.log(reversed);
 
     // .....................................................................................
 
 // Q4
 
-// function removeDuplicates(arr) {
-//     return [...new Set(arr)];
-// }  
-//     const numbers = [1, 2, 3, 2, 4, 1, 5];
-//     const uniqueNumbers = removeDuplicates(numbers);
-//     console.log(uniqueNumbers); // Output: [1, 2, 3, 4, 5]
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}  
+    const numbers3 = [1, 2, 3, 2, 4, 1, 5];
+    const uniqueNumbers = removeDuplicates(numbers3);
+    console.log(uniqueNumbers); // Output: [1, 2, 3, 4, 5]
